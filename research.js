a39d9c38db1b09bff18338d6102a15cb798be494
Vielo <radoslawjakubiak@gmail.com>
2018-01-18 23:56:05 +0100

    Fixed no tooltip for Laboratory research (function couldn't handle more than 1 instance of the same element id).

--- research.js
+++ research.js
@@ -3,7 +3,7 @@ var researchTierOneList = [
 		name: "Laboratory",
 		cost: 100,
 		unlocked: false,
-		codeName: 'laboratory',
+		codeName: 'res_laboratory',
 		fluffText: 'Making your employees do research during breaks and after hours is good and all, but you cannot expect them to match qualified scientists. Research this to begin your journey towards the Rockier Future.',
 		icon: 'img/research/icon_laboratory.png',
 		duration: 6000 // 60 seconds
@@ -14,7 +14,7 @@ var researchTierOneList = [
 		unlocked: false,
 		level: 0,
 		levelBonus: 0.2,
-		codeName: 'researchSpeedI',
+		codeName: 'res_researchSpeedI',
 		fluffText: 'Provides further training to your scientists, allowing them to make progress more swiftly.',
 		icon: 'img/research/icon_researchSpeedI.png',
 		duration: 120000 // 120 seconds
