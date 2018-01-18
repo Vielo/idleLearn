6ecc9ec2478d8fb21010aed363fd9b6be90b878c
Vielo <radoslawjakubiak@gmail.com>
2018-01-18 23:42:25 +0100

    Added icons to perSec display, added Lab, Steel Mill and Chemical Plant, added new Science icon

--- buildings.js
+++ buildings.js
@@ -83,7 +83,7 @@ var buildingsList = [
 			oil: 0,
 			plastics: 0,
 			circuits: 0,
-			science: 0
+			science: 0,
 		},
 		stuffPerTick: {
 			minerals: 0,
@@ -91,12 +91,84 @@ var buildingsList = [
 			oil: 200,
 			plastics: 0,
 			circuits: 0,
-			science: 0
+			science: 0,
 		},
 		fluffText: "<p>Every Seabed Mine will automatically produce 500 stone per second.</p><p>Seabed rock and mineral deposits have endured unimaginable pressures for aeons. They are compressed beyond measure.</p>",
 		totalAmount: 0,
 		codeName: "pumpJack",
-		tickSpeed: 7500
+		tickSpeed: 7500,
+	},
+	laboratory = {
+		name: "Laboratory",
+		namePlural: "Laboratories",
+		initialCost: {
+			minerals: 500,
+			steel: 0,
+			oil: 100,
+			plastics: 0,
+			circuits: 0,
+			science: 0,
+		},
+		stuffPerTick: {
+			minerals: 0,
+			steel: 0,
+			oil: 0,
+			plastics: 0,
+			circuits: 0,
+			science: 50,
+		},
+		fluffText: "<p>Each Laboratory will automatically produce 50 science every 10 seconds.</p><p>Laboratories are awesome!</p>",
+		totalAmount: 0,
+		codeName: "laboratory",
+		tickSpeed: 10000
+	},
+	steelMill = {
+		name: "Steel Mill",
+		namePlural: "Steel Mills",
+		initialCost: {
+			minerals: 10000,
+			steel: 0,
+			oil: 1000,
+			plastics: 0,
+			circuits: 0,
+			science: 0,
+		},
+		stuffPerTick: {
+			minerals: 0,
+			steel: 500,
+			oil: 0,
+			plastics: 0,
+			circuits: 0,
+			science: 0
+		},
+		fluffText: "<p>Each Steel Mill will automatically produce 500 steel every 5 seconds.</p><p>Need to rework the functions to allow for resource conversion so that the Steel Mill will consume Minerals to produce Steel, and in the future add an energy dependency!</p>",
+		totalAmount: 0,
+		codeName: "steelMill",
+		tickSpeed: 5000
+	},
+	chemicalPlant = {
+		name: "Chemical Plant",
+		namePlural: "Chemical Plants",
+		initialCost: {
+			minerals: 20000,
+			steel: 10000,
+			oil: 5000,
+			plastics: 0,
+			circuits: 0,
+			science: 0,
+		},
+		stuffPerTick: {
+			minerals: 0,
+			steel: 0,
+			oil: 0,
+			plastics: 100,
+			circuits: 0,
+			science: 0
+		},
+		fluffText: "<p>Each Chemical Plant will automatically produce 100 Plastics every 3 seconds.</p><p>Need to rework the functions to allow for resource conversion so that the Chemical Plant will consume Oil (possibly also some Minerals?) to produce Plastics, and in the future add an energy dependency!</p>",
+		totalAmount: 0,
+		codeName: "chemicalPlant",
+		tickSpeed: 3000
 	}
 ]
 
